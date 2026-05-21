28541d5ae5b2e78470eff5b8b749ad934b607b91
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2026-05-21 10:37:34 +0700

    cumulative market, strategy dan return

--- eurusd_x.py
+++ eurusd_x.py
@@ -23,6 +23,16 @@ data.loc[data['MA20'] > data['MA50'], 'Signal'] = 1
 data.loc[data['MA20'] < data['MA50'], 'Signal'] = -1
 
 
+# Hitung return market
+data['Return'] = data['Close'].pct_change()
+
+# Return strategy
+data['Strategy_Return'] = data['Return'] * data['Signal'].shift(1)
+
+# Cumulative return
+data['Cumulative_Market'] = (1 + data['Return']).cumprod()
+data['Cumulative_Strategy'] = (1 + data['Strategy_Return']).cumprod()
+
 ## Lihat Hasil Signal
 print(data[['Close', 'MA20', 'MA50', 'Signal']].tail(20))
 
@@ -33,6 +43,8 @@ plt.figure(figsize=(14,7))
 plt.plot(data['Close'], label='Close Price')
 plt.plot(data['MA20'], label='MA20')
 plt.plot(data['MA50'], label='MA50')
+plt.plot(data['Cumulative_Market'], label='Buy & Hold')
+plt.plot(data['Cumulative_Strategy'], label='Strategy')
 
 plt.legend()
 plt.title('Moving Average Crossover Strategy')
