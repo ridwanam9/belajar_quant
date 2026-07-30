1294da9521b5cfcfe68b0a0446022278f30f89dd
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2026-07-30 17:46:55 +0700

    cek data duplikat

--- data_cleaning.py
+++ data_cleaning.py
@@ -8,10 +8,20 @@ print("-"*50)
 print(df.info())
 print("-"*50)
 print(df.describe())
-print("-"*50)
 
 
 
 
 # berapa jumlah data hilang berdasarkan kolom
+print("-"*50)
+print("berapa banyak data hilang")
 print(df.isnull().sum())
+
+
+
+
+
+print("-"*50)
+print("data duplikat")
+# print(df.duplicated())
+print(df.duplicated().sum())
